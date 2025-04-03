1cfc88f8a6d1921419f41c361be1fc628c325595
Harshit-bettary <bharsha213756@gmail.com>
2025-04-03 14:20:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-dbConnect();
 
 const startServer = async () => {
   try {
